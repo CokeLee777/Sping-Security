e8c23a5294ca52cc918e2c9e659336cb918f4d36
CokeLee777 <akak640@kyonggi.ac.kr>
2022-08-28 20:12:16 +0900

    불필요한 코드 제거

--- corespringsecurity/src/main/java/io/security/corespringsecurity/util/WebUtil.java
+++ corespringsecurity/src/main/java/io/security/corespringsecurity/util/WebUtil.java
@@ -3,6 +3,7 @@ package io.security.corespringsecurity.util;
 import javax.servlet.http.HttpServletRequest;
 
 public class WebUtil {
+
     private static final String XML_HTTP_REQUEST = "XMLHttpRequest";
     private static final String X_REQUESTED_WITH = "X-Requested-With";
 
